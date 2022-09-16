9f563256a6f0fca9272a902f6ad48a2eabd6ef0f
Giovanni Ricca <giovanniricca@proton.me>
2022-09-16 17:39:23 +0200

    redwood: Inherit from sm8350-common
    Change-Id: I9d866d2b9144b08212573fa8a0810d9f29e2e7a6

---
+++ lineage_redwood.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from redwood device
+$(call inherit-product, device/xiaomi/redwood/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := redwood
+PRODUCT_MANUFACTURER := POCO
+PRODUCT_MODEL := redwood
+PRODUCT_NAME := lineage_redwood
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redwood_global-user 14 UKQ1.240624.001 OS2.0.3.0.UMSMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := POCO/redwood_global/redwood:14/UKQ1.240624.001/OS2.0.3.0.UMSMIXM:user/release-keys
