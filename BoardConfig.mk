78ad73b16e385d22accff45bd6df3061e37393a4
TheStrechh <TheStrechh@gmail.com>
2023-08-02 00:33:56 +0530

    redwood: Use `AtomX` Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,13 +16,11 @@ AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER := true
 TARGET_BOOTLOADER_BOARD_NAME := redwood
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/redwood_QGKI.config
+TARGET_KERNEL_CONFIG += vendor/redwood-fragment.config
 
 # Kernel modules
 BOOT_KERNEL_MODULES := \
     goodix_core.ko \
-    hwid.ko \
-    msm_drm.ko \
     xiaomi_touch.ko
 
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
