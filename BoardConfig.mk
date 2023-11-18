04a5dd1c30dffae08fb1c3e336505df40b514fd9
TheStrechh <TheStrechh@gmail.com>
2023-11-19 00:31:38 +0530

    redwood: Partially import prebuilt kernel stuff * Note - Use Lineage kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,16 +17,17 @@ include device/xiaomi/sm7325-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := redwood
 
-# Kernel
-TARGET_KERNEL_CONFIG += vendor/redwood-fragment.config
-
-# Kernel modules
-BOOT_KERNEL_MODULES := \
-    goodix_core.ko \
-    xiaomi_touch.ko
-
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
+# Prebuilt Kernel
+TARGET_FORCE_PREBUILT_KERNEL := true
+BOARD_KERNEL_BINARIES := kernel
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/kernel/dtbo.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel/kernel
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/kernel/dtb.img
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/prebuilt/kernel/dtb.img:$(TARGET_COPY_OUT)/dtb.img \
+    $(DEVICE_PATH)/prebuilt/kernel/kernel:kernel \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/prebuilt/kernel/ramdisk-modules/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/prebuilt/kernel/vendor-modules/,$(TARGET_COPY_OUT_VENDOR)/lib/modules)
 
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
