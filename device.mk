de694f24d4ad4842fc947b1b75f80ae602e39534
Alexander Winkowski <dereference23@outlook.com>
2024-01-08 07:05:38 +0000

    redwood: Add SKU props from stock
    Change-Id: Ib7b6a0d8b992f264762c21f477c42f2114e51c7f

--- device.mk
+++ device.mk
@@ -41,6 +41,10 @@ PRODUCT_ENABLE_UFFD_GC := false
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.redwood.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.redwood.rc
 
+# Sku properties
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/sku/,$(TARGET_COPY_OUT_ODM)/etc)
+
 # WiFi Display
 PRODUCT_SYSTEM_PROPERTIES += \
     vendor.sys.video.disable.ubwc=1
