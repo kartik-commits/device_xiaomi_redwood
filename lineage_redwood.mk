4290cd5c665fe35b8e9d86f8c24cb496aebf0ae9
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    redwood: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_redwood.mk
+++ lineage_redwood.mk
@@ -23,7 +23,5 @@ PRODUCT_NAME := lineage_redwood
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redwood_global-user 14 UKQ1.240624.001 OS2.0.3.0.UMSMIXM release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := POCO/redwood_global/redwood:14/UKQ1.240624.001/OS2.0.3.0.UMSMIXM:user/release-keys
+    BuildDesc="redwood_global-user 14 UKQ1.240624.001 OS2.0.3.0.UMSMIXM release-keys" \
+    BuildFingerprint=POCO/redwood_global/redwood:14/UKQ1.240624.001/OS2.0.3.0.UMSMIXM:user/release-keys
