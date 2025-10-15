5c30d3221ffdb8fd07b6f5b589699b67b2a151fd
kartik-commits <kartiklashkare@gmail.com>
2025-10-15 21:47:12 +0530

    redwood: Adapt for `PixelOS` + Update from OS2.0.10.0.UMSMIXM

--- custom_redwood.mk
+++ custom_redwood.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from redwood device
 $(call inherit-product, device/xiaomi/redwood/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := redwood
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 22101320C
-PRODUCT_NAME := lineage_redwood
+PRODUCT_NAME := custom_redwood
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -26,9 +26,9 @@ PRODUCT_SYSTEM_NAME := redwood
 PRODUCT_SYSTEM_DEVICE := redwood
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redwood-user 12 RKQ1.211001.001 V14.0.1.0.TMSMIXM release-keys" \
+    PRIVATE_BUILD_DESC="redwood-user 13 RKQ1.211001.001 OS2.0.10.0.UMSMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Redmi/redwood/redwood:12/RKQ1.211001.001/V14.0.1.0.TMSMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/redwood/redwood:13/RKQ1.211001.001/OS2.0.10.0.UMSMIXM:user/release-keys
